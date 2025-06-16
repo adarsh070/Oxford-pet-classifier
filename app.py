2f22e6b7e24333e61762d45b5e8bd131eb8687ba
Adarsh Kumar <adarshsan@users.noreply.huggingface.co>
2025-06-16 10:26:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ from fastai.vision.all import *
 import skimage
 import pickle
 
+
 with open('vocab.pkl', 'rb') as f:
-    labels = pickle.load(f)
+    raw_labels = pickle.load(f)
+
+human_labels = {False: "Dog", True: "Cat"}
+labels = [human_labels[l] for l in raw_labels]
 
 dls = ImageDataLoaders.from_lists(
     path=".", 
@@ -16,15 +20,23 @@ dls = ImageDataLoaders.from_lists(
     bs=1
 )
 
-learn = vision_learner(dls, resnet34, metrics=error_rate)
+model_path = Path.cwd()/'model'
+learn = vision_learner(dls, resnet18, metrics=error_rate, path=model_path)
 
-learn.load('model_weights.pth', with_path=True)
+
+learn.load('model_weights')
+
 
 
 def predict(img):
-   
+    """
+    Gets predictions and returns a dictionary of human-readable labels and probabilities.
+    """
     pred, pred_idx, probs = learn.predict(img)
+    # The 'labels' variable is now ['Dog', 'Cat'], which matches the order of 'probs'
     return {label: float(prob) for label, prob in zip(labels, probs)}
+
+
 
 title = "Pet Breed Classifier (Cat vs. Dog)"
 description = "A pet classifier trained on the Oxford Pets dataset with fastai. Upload an image of a cat or a dog to see the model's prediction."

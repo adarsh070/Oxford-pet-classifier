132311dcdd22997dc403a51a2f8eb5a78dab5f27
Adarsh Kumar <aadarsh0104@gmail.com>
2025-06-16 14:14:22 +0530

    Add application file

---
+++ app.py
@@ -0,0 +1,35 @@
+
+
+import gradio as gr
+from fastai.vision.all import *
+import skimage
+
+learn = load_learner('model.pkl', cpu=True)
+
+labels = learn.dls.vocab
+
+def predict(img):
+    """
+    Takes a PIL image, gets predictions from the fastai learner,
+    and returns a dictionary of labels and their probabilities.
+    """
+    pred, pred_idx, probs = learn.predict(img)
+    
+    return {label: float(prob) for label, prob in zip(labels, probs)}
+
+
+
+title = "Pet Breed Classifier (Cat vs. Dog)"
+description = "A pet classifier trained on the Oxford Pets dataset with fastai. Upload an image of a cat or a dog to see the model's prediction."
+article = "<p style='text-align: center'><a href='https://www.robots.ox.ac.uk/~vgg/data/pets/' target='_blank'>Oxford-IIIT Pet Dataset</a> | <a href='https://github.com/fastai/fastbook' target='_blank'>Fast.ai course</a></p>"
+
+gr.Interface(
+    fn=predict, 
+    inputs=gr.Image(height=224, width=224), 
+    outputs=gr.Label(num_top_classes=2),
+    title=title,
+    description=description,
+    article=article,
+    examples=['cat_example.jpg'], 
+    allow_flagging="never"
+).launch()
